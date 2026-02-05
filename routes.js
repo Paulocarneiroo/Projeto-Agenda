79e8b2bf75c247a18e5cfbbbf78a5754fcbdacd8
Paulocarneiroo <paulocarneirosegundo@gmail.com>
2026-02-05 16:18:24 -0300

    Projeto Agenda

---
+++ routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const route = express.Router();
+const homeController = require('./src/controllers/homeController');
+const loginController = require('./src/controllers/loginController');
+const contactController = require('./src/controllers/contactController');
+const { loginRequired } = require('./src/middlewares/middleware');
+
+// Rotas da home
+route.get('/', homeController.index);
+
+//Rotas de login
+route.get('/login', loginController.index);
+route.post('/login/register', loginController.register);
+route.post('/login/login', loginController.login);
+route.get('/login/logout', loginController.logout);
+
+//Rotas de contact
+route.get('/contact', loginRequired, contactController.index);
+route.post('/contact/register', loginRequired, contactController.register);
+route.get('/contact/:id', loginRequired, contactController.editIndex);
+route.post('/contact/edit/:id', loginRequired, contactController.edit);
+module.exports = route;
---
+++ src/controllers/contactController.js
@@ -0,0 +1,55 @@
+const Contact = require('../models/ContactModel')
+const ContactModel = require("../models/ContactModel");
+
+exports.index = (req, res) => {
+  res.render('contact', {
+    contact: {}
+  });
+};
+
+exports.register = async (req, res) => {
+  try {
+    const contact = new Contact(req.body);
+    await contact.register();
+
+    if(contact.errors.length > 0){
+      req.flash('errors', contact.errors);
+      req.session.save(() => res.redirect('/contact'));
+      return;
+    }
+
+    req.flash('success', 'Contato registrado com sucesso');
+    req.session.save(() => res.redirect(`/contact/${contact.contact._id}`));
+    return;
+  }catch (e){
+    console.error(e);
+    return res.render('404');
+  }
+};
+
+exports.editIndex = async (req, res) => {
+  if(!req.params.id) return res.render('404');
+  const contact = await Contact.findById(req.params.id);
+
+  if (!contact) return res.render('404');
+  res.render('contact', { contact });
+}
+
+exports.edit = async (req, res) => {
+  try {
+    if(!req.params.id) return res.render('404');
+    const contact = new Contact(req.body);
+    await contact.edit(req.params.id);
+    if(contact.errors.length > 0){
+      req.flash('errors', contact.errors);
+      req.session.save(() => res.redirect('/contact'));
+      return;
+    }
+    req.flash('success', 'Contato editado com sucesso.');
+    req.session.save(() => res.redirect(`/contact/${contact.contact._id}`));
+    return;
+  }catch (e){
+    console.error(e);
+    return res.render('404');
+  }
+};
